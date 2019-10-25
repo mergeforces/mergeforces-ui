db8577b7d673d7302693db2b7a0ef2c8ee0ed1e4
mattbudde <matt@designbymatt.org>
2019-10-25 18:41:08 +0100

    Hero image boilerplate

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
+import bg from '../static/hero.jpg';
+
 const Hero = () => {
+  const background = {
+    backgroundImage: `url(${bg})`,
+    width: '100%',
+    height: '500px',
+  }
+
   return (
     <React.Fragment>
-      <div class="pt-24 bg-fixed">
-        <div class="container px-4 mx-auto">
-          <div class="">
+      <div class="bg-cover" style={background}>
+        <div className="container px-4 mx-auto pt-24 bg-local">
+          <div class="flex-1 text-center">
             <h1 class="my-4 text-5xl font-bold leading-tight">Main Hero Message to sell yourself!</h1>
             <p class="leading-normal text-2xl mb-8">Sub-hero message, not too long and not too short. Make it just right!</p>
             <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </React.Fragment >
   )
 }
 
