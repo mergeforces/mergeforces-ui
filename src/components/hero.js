688fd297262f346a28a8c13b48e2ad12178bfc3a
Artemiy Knipe | Артемий Кондратьев <artemiy.kondratiev@gmail.com>
2019-10-26 18:12:04 +0100

    Amend landing page header
    Co-Authored-By: Kyle Harrison <kyle90adam@hotmail.com>

--- src/components/hero.js
+++ src/components/hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
       <div className="bg-cover" style={background}>
         <div className="container px-4 mx-auto pt-24 bg-local">
           <div className="flex-1 text-center">
-            <h1 className="my-4 text-5xl font-bold leading-tight">Get together with friends</h1>
+            <h1 className="my-4 text-5xl font-bold leading-tight">Get together with friends!</h1>
             <p className="leading-normal text-2xl mb-8">Create one-off events without a signup process.</p>
             <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Create Event</button>
           </div>
