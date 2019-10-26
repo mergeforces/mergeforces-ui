00ee88ccf1c81ad647e2f6eed87905e19a26497d
mattbudde <matt@designbymatt.org>
2019-10-26 10:35:23 +0100

    Feedback

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-import bg from '../static/hero.jpg';
+import heroImage from '../static/hero.jpg';
 
 const Hero = () => {
   const background = {
-    backgroundImage: `url(${bg})`,
+    backgroundImage: `url(${heroImage})`,
     width: '100%',
     height: '500px',
   }
 
   return (
     <React.Fragment>
-      <div class="bg-cover" style={background}>
+      <div className="bg-cover" style={background}>
         <div className="container px-4 mx-auto pt-24 bg-local">
-          <div class="flex-1 text-center">
-            <h1 class="my-4 text-5xl font-bold leading-tight">Main Hero Message to sell yourself!</h1>
-            <p class="leading-normal text-2xl mb-8">Sub-hero message, not too long and not too short. Make it just right!</p>
-            <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
+          <div className="flex-1 text-center">
+            <h1 className="my-4 text-5xl font-bold leading-tight">Main Hero Message to sell yourself!</h1>
+            <p className="leading-normal text-2xl mb-8">Sub-hero message, not too long and not too short. Make it just right!</p>
+            <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
           </div>
         </div>
       </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,11 +5,11 @@ import logo from '../static/logo.png'
 export const Menu = () => {
   return (
     <nav>
-      <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">About</a></li>
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Meetups</a></li>
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Login</a></li>
-        <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Register</a></li>
+      <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">About</a></li>
+        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Meetups</a></li>
+        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Login</a></li>
+        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Register</a></li>
       </ul>
     </nav>
   )
@@ -18,19 +18,19 @@ export const Menu = () => {
 const NavBar = () => {
   return (
     <React.Fragment>
-      <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2 shadow-md">
-        <div class="flex-1 flex justify-between items-center">
+      <header className="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2 shadow-md">
+        <div className="flex-1 flex justify-between items-center">
           <a href="#">
             <img src={logo}></img>
           </a>
         </div>
-        <label for="menu-toggle" class="pointer-cursor lg:hidden block">
-          <svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <label htmlFor="menu-toggle" className="pointer-cursor lg:hidden block">
+          <svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </label>
-        <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+        <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
           <Menu />
         </div>
       </header>
