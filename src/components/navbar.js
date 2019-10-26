9a457e281faa9f3a6a9a2f1e67776974c6da649a
Artemiy Knipe | Артемий Кондратьев <artemiy.kondratiev@gmail.com>
2019-10-26 18:13:43 +0100

    Amend Landing Page Text and Navigation Links (#8)
    * Update landing page text
    
    * Remove placeholder links for signups and meetup references
    
    * Amend landing page header
    
    Co-Authored-By: Kyle Harrison <kyle90adam@hotmail.com>

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,9 +7,6 @@ export const Menu = () => {
     <nav>
       <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
         <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">About</a></li>
-        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Meetups</a></li>
-        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Login</a></li>
-        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">Register</a></li>
       </ul>
     </nav>
   )
@@ -34,7 +31,7 @@ const NavBar = () => {
           <Menu />
         </div>
       </header>
-    </React.Fragment >
+    </React.Fragment>
   )
 }
 
