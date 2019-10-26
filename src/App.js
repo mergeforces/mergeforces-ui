9ddb83ec9107178ee190629f0957dbf5d230ca8e
Artemiy Knipe | Артемий Кондратьев <artemiy.kondratiev@gmail.com>
2019-10-26 19:42:59 +0100

    Add client-side router and a placeholder for the "About" page (#10)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import NavBar from './components/navbar';
 import Hero from './components/hero';
+import About from './components/about';
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Hero />
+      <Router>
+        <NavBar />
+        <Route exact path='/'>
+          <Hero />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+      </Router>
     </div>
   );
 }
