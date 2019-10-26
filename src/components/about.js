8bb964141f121b64147f83555494533e953fe31a
awflwafl <art@shopkeep.com>
2019-10-26 19:17:09 +0100

    Add client-side router and a placeholder for the "About" page

---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <header>
+        <h1>About (placeholder)</h1>
+      </header>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </section>
+  );
+};
+
+export default About;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../static/logo.png'
 
@@ -6,7 +7,9 @@ export const Menu = () => {
   return (
     <nav>
       <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-        <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" href="#">About</a></li>
+        <li>
+          <Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-red-400" to='/about'>About</Link>
+        </li>
       </ul>
     </nav>
   )
@@ -17,9 +20,9 @@ const NavBar = () => {
     <React.Fragment>
       <header className="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2 shadow-md">
         <div className="flex-1 flex justify-between items-center">
-          <a href="#">
+          <Link to='/'>
             <img src={logo}></img>
-          </a>
+          </Link>
         </div>
         <label htmlFor="menu-toggle" className="pointer-cursor lg:hidden block">
           <svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
