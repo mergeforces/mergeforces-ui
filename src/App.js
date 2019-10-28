3be6dd556ab7402cd744534f5ca1a02c3290b72d
awflwafl <art@shopkeep.com>
2019-10-28 11:34:56 +0000

    Draft the event creation and RSVP journey

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/navbar';
 import Hero from './components/hero';
 import About from './components/about';
+import CreateEvent from './components/create-event';
+import Event from './components/event';
+import RSVP from './components/rsvp';
 
 
 function App() {
@@ -16,6 +19,15 @@ function App() {
         </Route>
         <Route exact path='/about'>
           <About />
+        </Route>
+        <Route exact path='/create'>
+          <CreateEvent />
+        </Route>
+        <Route exact path='/event'>
+          <Event />
+        </Route>
+        <Route exact path='/rsvp'>
+          <RSVP />
         </Route>
       </Router>
     </div>
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import heroImage from '../static/hero.jpg';
 
@@ -17,7 +18,7 @@ const Hero = () => {
           <div className="flex-1 text-center">
             <h1 className="my-4 text-5xl font-bold leading-tight">Get together with friends!</h1>
             <p className="leading-normal text-2xl mb-8">Create one-off events without a signup process.</p>
-            <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Create Event</button>
+            <Link to='/create' className="mx-auto lg:mx-0 bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Create Event</Link>
           </div>
         </div>
       </div>
